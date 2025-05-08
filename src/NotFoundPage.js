f792b223dcde9ecd2aaea71a5f35b6f5bfe00589
Brett Fowler <b16fowler@gmail.com>
2025-05-07 20:46:25 -0400

    Test commit

--- src/NotFoundPage.js
+++ src/NotFoundPage.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function NotFoundPage() {
   return (
     <div>
-      <h1>404 not found :/</h1>
+      <h1>404 this page not found :/</h1>
       <Link to="/">Return to main menu</Link>
     </div>
   );
