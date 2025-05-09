9a3435fa70a0ca91d35902b9c19aa8496f15ad1d
Brett Fowler <b16fowler@gmail.com>
2025-05-09 14:59:32 -0400

    Start of table implementation for ViewExercisesPage

--- src/pages/ViewExercisesPage.js
+++ src/pages/ViewExercisesPage.js
@@ -1,7 +1,14 @@
+import ExerciseChart from "../ExerciseTable";
+
 export default function ViewExercises() {
   return (
-    <>
+    <div>
       <h1 className="main-menu-header">ViewExercisesPage component</h1>
-    </>
+      <br />
+      <br />
+      <br />
+      <br />
+      <ExerciseChart />
+    </div>
   );
 }
