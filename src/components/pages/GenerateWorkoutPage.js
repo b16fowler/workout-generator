823d795465649e6292f4b818a5ba8d1b83490840
Brett Fowler <b16fowler@gmail.com>
2025-05-18 13:22:26 -0400

    Refactored logic for validating submission checkboxes

--- src/components/pages/GenerateWorkoutPage.js
+++ src/components/pages/GenerateWorkoutPage.js
@@ -12,74 +12,64 @@ export default function GenerateWorkout() {
     <>
       <h1 className="main-menu-header">GenerateWorkoutPage component</h1>
       <br />
-      <h3 id="generate-page">
-        Select all target muscles to be included in workout
-      </h3>
-      <form id="generate-page" onSubmit={handleSubmit}>
-        <input type="checkbox" id="arms" name="arms" />
-        <label htmlFor="arms">Arms </label>
-        <input type="checkbox" id="back" name="back" />
-        <label htmlFor="back">Back </label>
-        <input type="checkbox" id="legs" name="legs" />
-        <label htmlFor="legs">Legs </label>
-        <input type="checkbox" id="core" name="core" />
-        <label htmlFor="core">Core </label>
-        <br />
-        <br />
-        <label htmlFor="numExercises">Number of exercises in workout: </label>
-        <input
-          type="number"
-          id="numExercises"
-          placeholder="Leave blank for all"
-        ></input>
-        <br />
-        <br />
-        <input
-          className="button"
-          type="Submit"
-          value="Generate workout"
-          onChange={handleSubmit}
-        />
-        <br />
-        <br />
-        <h4 id="generate-page">To select all types, select none</h4>
-      </form>
-      <ReturnHome />
+      <div id="generate-page">
+        <h3>Select all target muscles to be included in workout</h3>
+        <form className="form" onSubmit={handleSubmit}>
+          <input type="checkbox" id="arms" name="box" />
+          <label htmlFor="arms">Arms </label>
+          <input type="checkbox" id="back" name="box" />
+          <label htmlFor="back">Back </label>
+          <input type="checkbox" id="legs" name="box" />
+          <label htmlFor="legs">Legs </label>
+          <input type="checkbox" id="core" name="box" />
+          <label htmlFor="core">Core </label>
+          <br />
+          <br />
+          <label htmlFor="numExercises">Number of exercises in workout: </label>
+          <input
+            type="number"
+            id="numExercises"
+            placeholder="Leave blank for all"
+          ></input>
+          <br />
+          <br />
+          <input
+            className="button"
+            type="Submit"
+            value="Generate workout"
+            onChange={handleSubmit}
+          />
+          <br />
+          <br />
+          <h4>To select all types, select none</h4>
+        </form>
+        <ReturnHome />
+      </div>
     </>
   );
 }
 
-// handleSubmit called when form submitted
 // workout will contain all exercises based on which types were checked
 function handleSubmit(e) {
-  // TODO: import FormData ?
-
-  // Prevent page of reloading on submission
   e.preventDefault();
 
-  // Pull boolean form values
-  let formValues = {
-    arms: document.querySelector("#arms").checked,
-    back: document.querySelector("#back").checked,
-    legs: document.querySelector("#legs").checked,
-    core: document.querySelector("#core").checked,
-    num: document.querySelector("#numExercises").value,
-  };
+  // Pull checkbox boolean values and assign to new object typeCheckboxes
+  const formValues = document.getElementsByName("box");
+  const typeCheckboxes = {};
+  formValues.forEach((value) => {
+    typeCheckboxes[value.id] = value.checked;
+  });
 
   // If all checkboxes left blank, select all
-  const noneChecked = Object.values(formValues).every((value) => !value);
+  const noneChecked = Object.values(typeCheckboxes).every((value) => !value);
   if (noneChecked) {
-    formValues = {
-      arms: true,
-      back: true,
-      legs: true,
-      core: true,
-    };
+    Object.keys(typeCheckboxes).forEach((key) => {
+      typeCheckboxes[key] = true;
+    });
   }
 
   const workout = userExercises.filter((exercise) => {
-    // Add exercise if type was checked
-    return formValues[exercise.type.toLowerCase()];
+    return typeCheckboxes[exercise.type.toLowerCase()];
   });
 
   displayWorkout(workout);
