76c499d1558660ebbf7bb8709cfebdaa240988f7
Brett Fowler <b16fowler@gmail.com>
2025-05-23 14:41:59 -0400

    New Clock component to display current time on all headers

---
+++ src/components/Clock.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+export default function LiveClock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString();
+
+  return <p id="clock">{formattedTime}</p>;
+}
