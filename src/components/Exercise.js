3786d833f81cff5c5f81ff15c051d1387758d967
Brett Fowler <b16fowler@gmail.com>
2025-05-24 16:08:07 -0400

    Fix for issue causing exercise component to render twice

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,29 +1,41 @@
+/**************************************************************************
+ * Exercise component
+ **************************************************************************/
+
 export default function Exercise({ workout, index }) {
   // Create HTML element for current exercise
   showExercise(workout[index], index);
 }
 
 function showExercise(exercise, index) {
-  // Create div to hold elements to be displayed
-  let div = document.createElement("div");
-  div.id = index;
-  div.className = "exercise-div";
+  // Check if div with same ID exists already
+  const existingDiv = document.getElementById(index);
 
-  // Create image element
-  let img = document.createElement("img");
-  img.className = "exercise-image";
-  img.src = exercise.pic;
-  img.alt = exercise.name;
-  img.style.display = "block";
+  // Not sure why this is needed. For some reason after the API call is added
+  // in Workout.js, this div is rendering twice
+  if (!existingDiv) {
+    console.log("Div for given index does not exist yet");
+    // Create div to hold elements to be displayed
+    let div = document.createElement("div");
+    div.id = index;
+    div.className = "exercise-div";
 
-  // Create exercise number label
-  let exerciseNum = document.createElement("h1");
-  exerciseNum.innerText = `Exercise #${index + 1}: ${exercise.name}`;
+    // Create image element
+    let img = document.createElement("img");
+    img.className = "exercise-image";
+    img.src = exercise.pic;
+    img.alt = exercise.name;
+    img.style.display = "block";
 
-  // Create reps/sets/label
-  let exerciseInfo = document.createElement("h1");
-  exerciseInfo.innerText = `${exercise.reps} reps for ${exercise.sets} sets`;
+    // Create exercise number label
+    let exerciseNum = document.createElement("h1");
+    exerciseNum.innerText = `Exercise #${index + 1}: ${exercise.name}`;
 
-  div.append(exerciseNum, img, exerciseInfo);
-  document.body.appendChild(div);
+    // Create reps/sets/label
+    let exerciseInfo = document.createElement("h1");
+    exerciseInfo.innerText = `${exercise.reps} reps for ${exercise.sets} sets`;
+
+    div.append(exerciseNum, img, exerciseInfo);
+    document.body.appendChild(div);
+  }
 }
