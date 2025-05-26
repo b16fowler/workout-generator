1e3ac41ecd8885813be3f0b2fa6de814ffd16b9c
Brett Fowler <b16fowler@gmail.com>
2025-05-25 20:27:50 -0400

    Creation of Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+/**************************************************************************
+ * Footer component
+ **************************************************************************/
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <p className="author-info">
+          Author: Brett Fowler
+          <br />
+          Website:{" "}
+          <a href="https://b16fowler.github.io/">https://b16fowler.github.io</a>
+        </p>
+      </footer>
+    </>
+  );
+}
