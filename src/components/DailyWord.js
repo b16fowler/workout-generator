4a7bb5adafe0779e305130c7c3db062dd793159c
Brett Fowler <b16fowler@gmail.com>
2025-05-29 12:37:03 -0400

    Fleshed out DailyWord component

--- src/components/DailyWord.js
+++ src/components/DailyWord.js
@@ -2,20 +2,40 @@
  * DailyWord component
  **************************************************************************/
 
+import { useSuspenseQuery } from "@tanstack/react-query";
+
 export default function DailyWord() {
   const API_KEY = "9051cdc0-cdd3-45b7-82ca-6bf5d75da762";
   const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=${API_KEY}`;
 
-  // fetch(url)
-  //   .then((response) => response.json())
-  //   .then((data) => console.log(data));
+  const response = useSuspenseQuery({
+    queryKey: ["dailyword"],
+    queryFn: () => fetch(url).then((response) => response.json()),
+  });
+
+  // Get word, then capitalize
+  let word = response.data[0].meta.id;
+  word = word[0].toUpperCase() + word.substring(1);
+
+  // Get definition, then capitalize
+  let def = response.data[0].shortdef[0];
+  def = def[0].toUpperCase() + def.substring(1);
+
+  // Get part of speech
+  let pos = response.data[0].fl;
 
   return (
-    <div>
+    <>
       <br />
       <br />
       <br />
-      <h2 id="generate-page">Word of the Day:</h2>
-    </div>
+      <div id="wotd-div">
+        <h2>
+          <strong>Word of the Day:</strong> <span id="word">{word}</span>{" "}
+          <span id="pos">({pos})</span>
+        </h2>
+        <p id="defintion">{def}</p>
+      </div>
+    </>
   );
 }
