b02dbc14e0b7b0f3be7a19a976b6a7eb39764a4d
Brett Fowler <b16fowler@gmail.com>
2025-07-07 13:28:13 -0400

    Small changes

--- src/server.js
+++ src/server.js
@@ -5,8 +5,10 @@
 import "dotenv/config";
 import mysql from "mysql2/promise";
 
+// Test query
 const query = "SELECT * FROM table1";
 
+// Create connection for db
 const connection = await mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -15,26 +17,29 @@ const connection = await mysql.createConnection({
   port: 3306,
 });
 
+// Establish connection
 connection.connect((err) => {
   if (err) {
     console.error("Error connecting to RDS:\n", err.code, "\n", err.message);
     return;
   }
-  console.log("Connection successful...");
+  console.log("Connection successful...\n");
 });
 
+// Run query
 console.log("Testing query\n");
 try {
   const [results] = await connection.query(query);
   console.log(results[0]);
 } catch (err) {
-  console.log(err);
+  console.log("Query error:\n", err);
 }
 
+// Close connection
 console.log("\nQuery finished. Closing connection...");
 connection.end((err) => {
   if (err) {
-    console.log("Error closing connection: ", err);
+    console.log("Error closing connection:\n", err);
   }
   console.log("Connection closing successfully");
 });
