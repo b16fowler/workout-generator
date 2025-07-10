4bcd4d2d895538c28804f900fa162740f0f78d9f
Brett Fowler <b16fowler@gmail.com>
2025-07-10 17:22:32 -0400

    Code format change

--- src/server.js
+++ src/server.js
@@ -13,6 +13,15 @@ const PORT = 5000;
 app.use(express.json());
 app.use(cors());
 
+// Create connection for db
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: 3306,
+});
+
 // Query options
 const login_query = "SELECT * FROM logins";
 
@@ -28,13 +37,4 @@ app.get("/api/login", async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`\nServer is running at http://localhost:${PORT}`);
-});
-
-// Create connection for db
-const connection = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: 3306,
 });
