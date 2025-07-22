fb25938a78bf6215d543b64f251366e3cb32c730
Brett Fowler <b16fowler@gmail.com>
2025-07-22 12:50:33 -0400

    New user object for keeping track of login

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,17 @@
 import LoginPage from "./LoginPage.js";
 import "../css/index.css";
 
+// Empty string when not logged in, set to username after log in
+const user = {
+  _name: "",
+  get name() {
+    return this._name;
+  },
+  set name(value) {
+    this._name = value;
+  },
+};
+
 // Hardcoded test data
 const userExercises = [
   {
@@ -73,4 +84,4 @@ export default function App() {
   );
 }
 
-export { userExercises };
+export { userExercises, user };
