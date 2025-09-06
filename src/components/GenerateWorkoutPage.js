d9d2df53491bfe3d5412f42ee5478eab9fa83be4
Brett Fowler <b16fowler@gmail.com>
2025-09-06 10:41:33 -0600

    Working on logic changes to generate workout from DB

--- src/components/GenerateWorkoutPage.js
+++ src/components/GenerateWorkoutPage.js
@@ -3,11 +3,12 @@
  **************************************************************************/
 
 import Workout from "./Workout.js";
-import { userExercises } from "./App.js";
+// import { userExercises } from "./App.js";
 import { root } from "../index.js";
 import Header from "./Header.js";
 import Footer from "./Footer.js";
 import ReturnHome from "./ReturnHome.js";
+import axios from "axios";
 
 export default function GenerateWorkout() {
   return (
@@ -17,6 +18,8 @@ export default function GenerateWorkout() {
       <br />
       <div id="generate-page">
         <h3>Select all target muscles to be included in workout</h3>
+        <h4>To select all types, select none</h4>
+        <br />
         <form className="form" onSubmit={handleSubmit}>
           <input type="checkbox" id="arms" name="box" />
           <label htmlFor="arms">Arms </label>
@@ -32,8 +35,7 @@ export default function GenerateWorkout() {
           <input
             type="number"
             id="numExercises"
-            placeholder="Leave blank for all"
-          ></input>
+            placeholder="Leave blank for all"></input>
           <br />
           <br />
           <input
@@ -44,7 +46,6 @@ export default function GenerateWorkout() {
           />
           <br />
           <br />
-          <h4>To select all types, select none</h4>
         </form>
       </div>
       <ReturnHome />
@@ -53,68 +54,95 @@ export default function GenerateWorkout() {
   );
 }
 
-// workout will contain all exercises based on which types were checked
 function handleSubmit(e) {
   e.preventDefault();
 
   // Pull checkbox boolean values and assign to new object typeCheckboxes
   const formValues = document.getElementsByName("box");
-  const typeCheckboxes = {};
-  formValues.forEach((value) => {
-    typeCheckboxes[value.id] = value.checked;
+  const checkboxes = {};
+  formValues.forEach(value => {
+    checkboxes[value.id] = value.checked;
   });
 
   // If all checkboxes left blank, check all
-  const noneChecked = Object.values(typeCheckboxes).every((value) => !value);
+  const noneChecked = Object.values(checkboxes).every(value => !value);
   if (noneChecked) {
-    Object.keys(typeCheckboxes).forEach((key) => {
-      typeCheckboxes[key] = true;
+    Object.keys(checkboxes).forEach(key => {
+      checkboxes[key] = true;
     });
   }
 
-  // Keep exercises of muscle types that were checked off
-  let workout = userExercises.filter((exercise) => {
-    return typeCheckboxes[exercise.type.toLowerCase()];
-  });
+  // Get number of desired exercises
+  let numExercises = document.querySelector("#numExercises").value;
+  // If number of exercises field left blank, assign 1 to it
+  // TODO: Fix
+  numExercises = numExercises ? numExercises : 1;
 
-  // Check for number of exercises desired in workout
-  const numExercises = document.querySelector("#numExercises").value;
-  // Inform user if there are not enough logged exercises to generate workout of desired length
-  if (workout.length < numExercises) {
-    alert(
-      `Desired number of exercises (${numExercises})\nexceeds number of logged exercises of selected type(s) (${workout.length})\nGenerating workout of max possible length (${workout.length})`
-    );
-  }
-  // Randomize and trim workout
-  workout = jumbleWorkout(workout, numExercises);
-
-  displayWorkout(workout);
+  root.render(<Workout numExercises={numExercises} checkboxes={checkboxes} />);
 }
 
-function displayWorkout(workout) {
-  // Hide rest of page to display image(s)
-  const toHide = document.querySelectorAll("#generate-page");
-  toHide.forEach((exercise) => {
-    exercise.setAttribute("hidden", "true");
-  });
+// workout will contain all exercises based on which types were checked
+// function handleSubmit(e) {
+//   e.preventDefault();
 
-  root.render(<Workout workout={workout} />);
-}
+//   // Pull checkbox boolean values and assign to new object typeCheckboxes
+//   const formValues = document.getElementsByName("box");
+//   const typeCheckboxes = {};
+//   formValues.forEach(value => {
+//     typeCheckboxes[value.id] = value.checked;
+//   });
 
-function jumbleWorkout(workout, numExercises) {
-  /* Shuffle exercises with Fisher-Yates shuffle algorithm
-   * Iterate through array of exercises backwards, swapping it's index
-   * with a randomly generated number */
-  for (let i = workout.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [workout[i], workout[j]] = [workout[j], workout[i]];
-  }
+//   // If all checkboxes left blank, check all
+//   const noneChecked = Object.values(typeCheckboxes).every(value => !value);
+//   if (noneChecked) {
+//     Object.keys(typeCheckboxes).forEach(key => {
+//       typeCheckboxes[key] = true;
+//     });
+//   }
 
-  // Check if a number of exercises was entered
-  if (numExercises) {
-    // Trim workout to fit number of exercises
-    workout = workout.slice(0, numExercises);
-  }
+//   // Keep exercises of muscle types that were checked off
+//   let workout = userExercises.filter(exercise => {
+//     return typeCheckboxes[exercise.type.toLowerCase()];
+//   });
 
-  return workout;
-}
+//   // Check for number of exercises desired in workout
+//   const numExercises = document.querySelector("#numExercises").value;
+//   // Inform user if there are not enough logged exercises to generate workout of desired length
+//   if (workout.length < numExercises) {
+//     alert(
+//       `Desired number of exercises (${numExercises})\nexceeds number of logged exercises of selected type(s) (${workout.length})\nGenerating workout of max possible length (${workout.length})`
+//     );
+//   }
+//   // Randomize and trim workout
+//   workout = jumbleWorkout(workout, numExercises);
+
+//   displayWorkout(workout);
+// }
+
+// function displayWorkout(workout) {
+//   // Hide rest of page to display image(s)
+//   const toHide = document.querySelectorAll("#generate-page");
+//   toHide.forEach(exercise => {
+//     exercise.setAttribute("hidden", "true");
+//   });
+
+//   root.render(<Workout workout={workout} />);
+// }
+
+// function jumbleWorkout(workout, numExercises) {
+//   /* Shuffle exercises with Fisher-Yates shuffle algorithm
+//    * Iterate through array of exercises backwards, swapping it's index
+//    * with a randomly generated number */
+//   for (let i = workout.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [workout[i], workout[j]] = [workout[j], workout[i]];
+//   }
+
+//   // Check if a number of exercises was entered
+//   if (numExercises) {
+//     // Trim workout to fit number of exercises
+//     workout = workout.slice(0, numExercises);
+//   }
+
+//   return workout;
+// }
