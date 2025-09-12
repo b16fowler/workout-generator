843a838cc6c84c8f95f492d9e185e7be899c1216
Brett Fowler <b16fowler@gmail.com>
2025-09-12 14:22:54 -0400

    Reverting name change to fit new rendering process

---
+++ src/components/Workout.js
@@ -0,0 +1,25 @@
+/**************************************************************************
+ * Workout component
+ **************************************************************************/
+
+import { useState } from "react";
+import PreviousButton from "./PreviousButton.js";
+import NextButton from "./NextButton.js";
+import FinishButton from "./FinishButton.js";
+import Footer from "./Footer.js";
+
+export default function Workout({ workout }) {
+  // Tracks index of given exercise in generated workout
+  // let [exercises, setExercises] = useState([]);
+  let [index, setIndex] = useState(0);
+
+  return (
+    <>
+      {/*<Header heading={"Generated Workout"} /> */}
+      <PreviousButton index={index} setIndex={setIndex} />
+      <NextButton index={index} setIndex={setIndex} length={workout.length} />
+      <FinishButton index={index} length={workout.length} />
+      <Footer />
+    </>
+  );
+}
