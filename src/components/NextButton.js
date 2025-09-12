0cc2ec03039b3886ba72137ab983857dd41ba4d7
Brett Fowler <b16fowler@gmail.com>
2025-09-12 12:56:24 -0400

    Small change in logic to match HTML element changes

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -7,16 +7,17 @@ export default function NextButton({ index, setIndex, length }) {
     <button
       className="next button"
       onClick={handleClick}
-      hidden={index === length - 1 ? true : false}
-    >
+      hidden={index === length - 1 ? true : false}>
       Next exercise
     </button>
   );
 
   function handleClick() {
-    // Remove current image before next one is displayed
-    document.body.removeChild(document.querySelector(".exercise-div"));
+    // Hide current image before displaying next
+    document.getElementById(index).toggleAttribute("hidden");
+    document.getElementById(index + 1).toggleAttribute("hidden");
 
+    // Update index
     setIndex(index + 1);
   }
 }
