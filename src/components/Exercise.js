39032b02d80be28e5ac095600232604159739349
Brett Fowler <b16fowler@gmail.com>
2025-09-13 20:28:37 -0400

    Simplifying Exercise component labeling

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -2,40 +2,46 @@
  * Exercise component
  **************************************************************************/
 
-export default function Exercise({ workout, index }) {
-  // Create HTML element for current exercise
-  showExercise(workout[index], index);
+export default function Exercise({ name, sets, reps }) {
+  return (
+    <>
+      <h1>{name}</h1>
+      <h4>
+        {sets} sets of {reps} reps
+      </h4>
+    </>
+  );
 }
 
-function showExercise(exercise, index) {
-  // Check if div with same ID exists already
-  const existingDiv = document.getElementById(index);
+// function showExercise(exercise, index) {
+//   // Check if div with same ID exists already
+//   const existingDiv = document.getElementById(index);
 
-  // Not sure why this is needed. For some reason after the API call is added
-  // in Workout.js, this div is rendered twice
-  if (!existingDiv) {
-    console.log("Div for given index does not exist yet");
-    // Create div to hold elements to be displayed
-    let div = document.createElement("div");
-    div.id = index;
-    div.className = "exercise-div";
+//   // Not sure why this is needed. For some reason after the API call is added
+//   // in Workout.js, this div is rendered twice
+//   if (!existingDiv) {
+//     console.log("Div for given index does not exist yet");
+//     // Create div to hold elements to be displayed
+//     let div = document.createElement("div");
+//     div.id = index;
+//     div.className = "exercise-div";
 
-    // Create image element
-    let img = document.createElement("img");
-    img.className = "exercise-image";
-    img.src = exercise.pic;
-    img.alt = exercise.name;
-    img.style.display = "block";
+//     // Create image element
+//     let img = document.createElement("img");
+//     img.className = "exercise-image";
+//     img.src = exercise.pic;
+//     img.alt = exercise.name;
+//     img.style.display = "block";
 
-    // Create exercise number label
-    let exerciseNum = document.createElement("h1");
-    exerciseNum.innerText = `Exercise #${index + 1}: ${exercise.name}`;
+//     // Create exercise number label
+//     let exerciseNum = document.createElement("h1");
+//     exerciseNum.innerText = `Exercise #${index + 1}: ${exercise.name}`;
 
-    // Create reps/sets/label
-    let exerciseInfo = document.createElement("h1");
-    exerciseInfo.innerText = `${exercise.reps} reps for ${exercise.sets} sets`;
+//     // Create reps/sets/label
+//     let exerciseInfo = document.createElement("h1");
+//     exerciseInfo.innerText = `${exercise.reps} reps for ${exercise.sets} sets`;
 
-    div.append(exerciseNum, img, exerciseInfo);
-    document.body.appendChild(div);
-  }
-}
+//     div.append(exerciseNum, img, exerciseInfo);
+//     document.body.appendChild(div);
+//   }
+// }
