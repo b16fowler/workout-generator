2e3479df7b8221ac063df3b4fca914b1ea09c537
Brett Fowler <b16fowler@gmail.com>
2025-09-18 11:38:59 -0400

    Moved showSnackbar function from ./index.js

--- src/components/App.js
+++ src/components/App.js
@@ -18,10 +18,23 @@ const user = {
 
 export default function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <>
+      <div className="App">
+        <LoginPage />
+      </div>
+      <div id="snackbar"></div>
+    </>
   );
+}
+
+export function showSnackbar(message) {
+  const sb = document.querySelector("#snackbar");
+  sb.textContent = message;
+  sb.className = "show";
+
+  setTimeout(function () {
+    sb.className = sb.className.replace("show", "");
+  }, 3000);
 }
 
 export { user };
