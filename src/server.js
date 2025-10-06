eb2104d372d69218e8be251b5aaef654aff4ec97
Brett Fowler <b16fowler@gmail.com>
2025-10-06 11:16:22 -0400

    Readding logs

--- src/server.js
+++ src/server.js
@@ -239,6 +239,9 @@ app.get("/api/user-table", async (req, res) => {
   const userTableQuery = "SELECT username FROM logins;";
   try {
     const userList = await pool.query(userTableQuery);
+    console.log(userList[0]);
+    console.log(userList[0][0]);
+
     res.json({ userList: userList[0][0], success: true });
     console.log("[SUCCESS] userTableQuery run successfully\n");
   } catch (err) {
