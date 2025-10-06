e335675a44aad3989403c0c695b03e3b1ac9ce65
Brett Fowler <b16fowler@gmail.com>
2025-10-06 19:16:31 -0400

    Skeleton of new AddDelete component

---
+++ src/components/AdminAddDelete.js
@@ -0,0 +1,17 @@
+/**************************************************************************
+ * AdminAddDelete component
+ **************************************************************************/
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function AdminAddDelete() {
+  //TODO: FIX ERROR WHEN ATTEMPTING LOGOUT FROM ADMIN ADD DELETE COMPONENT
+  return (
+    <>
+      <Header heading="Add/Delete User Accounts" />
+      <div>Hello, world</div>
+      <Footer />
+    </>
+  );
+}
