da6528f78f11ac06831f072a9d74b2b30befcbd6
Brett Fowler <b16fowler@gmail.com>
2025-10-07 09:12:53 -0400

    Adding id to exercise details

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -6,8 +6,8 @@ export default function Exercise({ name, sets, reps }) {
   // Simple component to display name, # sets, and # reps for current exercise
   return (
     <>
-      <h1>{name}</h1>
-      <h4>
+      <h1 id="exercise-title">{name}</h1>
+      <h4 id="exercise-details">
         {sets} sets of {reps} reps
       </h4>
     </>
