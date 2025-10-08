cf45be2b16d71a7c490e95077c1d8b0c9c264595
Brett Fowler <b16fowler@gmail.com>
2025-10-08 11:17:22 -0400

    New component for admins deleting accounts

---
+++ src/components/AdminDelete.js
@@ -0,0 +1,70 @@
+/**************************************************************************
+ * AdminAddDelete component
+ **************************************************************************/
+
+import Header from "./Header";
+import Footer from "./Footer";
+import ReturnHome from "./ReturnHome";
+import { EC2_URL } from "..";
+import { showSnackbar } from "./App";
+
+export default function AdminDelete() {
+  //TODO: FIX ERROR WHEN ATTEMPTING LOGOUT FROM ADMIN ADD DELETE COMPONENT
+
+  //TODO: COMBINE FUNCTIONALITY OF DELETE ACCOUNT + RESET PW
+
+  //TODO: PUSH SERVER CHANGES THEN TEST
+  const searchUser = e => {
+    e.preventDefault();
+
+    const usernameInput = document.querySelector("#delete-user-input").value;
+    console.log(usernameInput);
+
+    const fetchUser = async () => {
+      const response = await fetch(`${EC2_URL}/api/search`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: usernameInput,
+        }),
+      });
+      const result = await response.json();
+
+      if (result.success) {
+        // Confirm, then delete account
+        const confirm = window.confirm(
+          `Delete account "` + usernameInput + `"?`
+        );
+        if (confirm) deleteUser(usernameInput);
+      } else {
+        showSnackbar(`User "` + usernameInput + `" was not found in database`);
+      }
+    };
+    fetchUser();
+  };
+
+  const deleteUser = () => {
+    console.log("Confirmed");
+  };
+
+  return (
+    <>
+      <Header heading="Delete User Accounts" />
+      <form className="delete-user-form" onSubmit={searchUser}>
+        <label>Search user's name to delete their account</label>
+        <br />
+        <input type="text" id="delete-user-input"></input>
+        <input
+          type="Submit"
+          value="Search"
+          id="delete-user-search"
+          onChange={searchUser}
+        />
+      </form>
+      <ReturnHome />
+      <Footer />
+    </>
+  );
+}
