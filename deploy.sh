8f76081af242f9f3fec2b7853b94e8863dde92ab
Brett Fowler <b16fowler@gmail.com>
2025-10-27 09:08:16 -0400

    Fixed pathing errors

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,13 @@
 echo "Building project..."
 npm run build
 
-echo "Zipping build folder..."
-zip -r build.zip build
-
-echo "Uploading to EC2..."
-scp -i ../EC2/bf_keypair.pem build.zip ec2-user@3.80.211.117:/home/ec2-user/
+echo "Moving build/ to EC2..."
+scp -i ../EC2/bf_keypair.pem -r build ec2-user@3.80.211.117:/home/ec2-user/
 
 echo "Deploying on EC2..."
 ssh -i ../EC2/bf_keypair.pem ec2-user@3.80.211.117 << 'ENDSSH'
-  cd /home/ec2-user
-  unzip -o build.zip -d /var/www/my-react-app/
-  rm build.zip
+  sudo rm -r /var/www/my-react-app/build/
+  sudo mv ~/build /var/www/my-react-app/
+  pm2 restart server
   echo "Deployment complete!"
 ENDSSH
