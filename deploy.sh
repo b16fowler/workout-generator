70e898fe2faf3608ff1810af560a701e9daa9523
Brett Fowler <b16fowler@gmail.com>
2025-11-01 14:19:54 -0400

    Fixing SSH mistake

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "SSH into EC2 instance..."
-ssh -i ../EC2/bf_keypair.pem ec2-user@3.80.211.117
+ssh -i ../EC2/bf_keypair.pem ec2-user@3.80.211.117 << 'ENDSSH'
 
 echo "Pulling changes and restarting processes..."
   cd workout-generator
