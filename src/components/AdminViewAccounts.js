b3a0fea1b9d0d4c21f2fc91baffc82a9101ef92e
Brett Fowler <b16fowler@gmail.com>
2025-11-04 11:00:12 -0500

    Newly renamed component

---
+++ src/components/AdminViewAccounts.js
@@ -0,0 +1,22 @@
+/**************************************************************************
+ * AdminViewUsers component
+ *
+ * Simple component that returns the standard Header and Footer
+ * as well as a component table displaying all usernames
+ **************************************************************************/
+
+import Header from "./Header";
+import Footer from "./Footer";
+import UsersTable from "./AccountsTable";
+import ReturnHome from "./ReturnHome";
+
+export default function AdminViewAccounts() {
+  return (
+    <>
+      <Header heading="View Accounts" />
+      <UsersTable />
+      <ReturnHome />
+      <Footer />
+    </>
+  );
+}
