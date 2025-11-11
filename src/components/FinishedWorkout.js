185a0a0974290378ee5a80abdb9365c2bfd4a694
Brett Fowler <b16fowler@gmail.com>
2025-11-11 18:16:11 -0500

    New component to offer user option to save workout or return to MainMenu after workout is completed

---
+++ src/components/FinishedWorkout.js
@@ -0,0 +1,23 @@
+/**************************************************************************
+ * FinishedWorkout component
+ *
+ * Component acts as a page that is rendered when user completes a workout.
+ * This component currently holds only the ReturnHome button component and
+ * the SaveWorkoutButton component
+ **************************************************************************/
+
+import Footer from "./Footer";
+import Header from "./Header";
+import ReturnHome from "./ReturnHome";
+import SaveWorkoutButton from "./SaveWorkoutButton";
+
+export default function FinishedWorkout() {
+  return (
+    <>
+      <Header heading="Workout completed!" />
+      <SaveWorkoutButton />
+      <ReturnHome />
+      <Footer />
+    </>
+  );
+}
