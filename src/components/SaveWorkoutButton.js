fa0d62463e377bb4ab54f3c55e4bcff877172176
Brett Fowler <b16fowler@gmail.com>
2025-11-11 18:17:39 -0500

    New component that will eventually make post call to backend to save workout in new table

---
+++ src/components/SaveWorkoutButton.js
@@ -0,0 +1,40 @@
+/**************************************************************************
+ * SaveWorkoutButton component
+ *
+ * Component is visible when user is in the middle of a workout. Clicking
+ * this button will save the generated workout for the user to come back
+ * to another time
+ **************************************************************************/
+
+import { EC2_URL } from "..";
+import user from "./App.js";
+import axios from "axios";
+
+export default function SaveWorkoutButton({ workout }) {
+  const handleSaveWorkout = () => {
+    console.log("Save workout button clicked");
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(`${EC2_URL}/api/save-workout`, {
+          user: user.username,
+          workout: workout,
+        });
+
+        console.log(response);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  };
+
+  return (
+    <>
+      <button className="save-workout-button" onClick={handleSaveWorkout}>
+        Save workout to library
+      </button>
+    </>
+  );
+}
