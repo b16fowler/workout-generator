817fc92d90d489b43157d289f3c9c4551c759599
Brett Fowler <b16fowler@gmail.com>
2025-11-11 20:27:27 -0500

    Adding new buttons to page

--- src/components/pages/FinishedWorkoutPage.js
+++ src/components/pages/FinishedWorkoutPage.js
@@ -10,12 +10,16 @@ import Header from "../other/Header";
 import Footer from "../other/Footer";
 import ReturnHomeButton from "../buttons/ReturnHomeButton";
 import SaveWorkoutButton from "../buttons/SaveWorkoutButton";
+import ReloadWorkoutButton from "../buttons/ReloadWorkoutButton";
+import LoadNewWorkoutButon from "../buttons/LoadNewWorkout";
 
 export default function FinishedWorkoutPage() {
   return (
     <>
       <Header heading="Workout completed!" />
       <SaveWorkoutButton />
+      <LoadNewWorkoutButon />
+      <ReloadWorkoutButton />
       <ReturnHomeButton />
       <Footer />
     </>
