4b5cab442bf493de3037822bcb130bbf6419453d
Brett Fowler <b16fowler@gmail.com>
2025-12-16 16:34:40 -0600

    New buttons that are hidden until 'generate workout' button is pressed

--- src/frontend/components/pages/MainMenuPage.js
+++ src/frontend/components/pages/MainMenuPage.js
@@ -11,15 +11,16 @@ import { user } from "../App.js";
 import Footer from "../other/Footer.js";
 import Header from "../other/Header.js";
 import { root } from "../../../index.js";
+import GenerateWorkoutPage from "./GenerateWorkoutPage.js";
 import AddExercisePage from "./AddExercisePage.js";
 import AdminDelete from "../admin/AdminDelete.js";
 import ViewExercisesPage from "./ViewExercisesPage.js";
-import GenerateWorkoutPage from "./GenerateWorkoutPage.js";
 import AdminViewAccounts from "../admin/AdminViewAccounts.js";
 import AdminResetPassword from "../admin/AdminResetPassword.js";
 
 export default function MainMenuPage() {
   const [adminOn, setAdminOn] = useState(false);
+  const [splitButton, setSplitButton] = useState(false);
 
   // Called to render new component
   const handleClick = component => {
@@ -31,12 +32,29 @@ export default function MainMenuPage() {
     setAdminOn(!adminOn);
   };
 
+  /* Toggles between 'Generate Workout' button and 
+     'New workout'/'From library' buttons */
+  const toggleSplitButtons = () => {
+    setSplitButton(!splitButton);
+  };
+
+  /* Tried to use 'handleClick(<GenerateWorkoutPage/>)'. That 
+     syntax was causing the GenerateWorkoutPage to load without
+     showing the split buttons */
+  const handleNewWorkout = () => {
+    root.render(<GenerateWorkoutPage />);
+  };
+
+  const handleLoadWorkout = () => {
+    console.log("handleNewWorkout button clicked");
+  };
+
   return (
     <>
       <Header heading="Workout Generator App" />
       <div className="main-menu">
         <button
-          className="main-menu-button"
+          className="top-main-menu-button"
           onClick={
             adminOn
               ? () => handleClick(<AdminDelete />)
@@ -53,15 +71,32 @@ export default function MainMenuPage() {
           }>
           {adminOn ? "Reset Passwords" : "View Exercises"}
         </button>
-        <button
-          className="main-menu-button"
-          onClick={
-            adminOn
-              ? () => handleClick(<AdminViewAccounts />)
-              : () => handleClick(<GenerateWorkoutPage />)
-          }>
-          {adminOn ? "View Accounts" : "Generate Workout"}
-        </button>
+        {!splitButton && (
+          <button
+            className="main-menu-button"
+            onClick={
+              adminOn
+                ? () => handleClick(<AdminViewAccounts />)
+                : () => toggleSplitButtons()
+            }>
+            {adminOn ? "View Accounts" : "Generate Workout"}
+          </button>
+        )}
+        {splitButton && (
+          <div className="split-button-wrapper">
+            <button className="split-button" onClick={handleNewWorkout}>
+              New workout
+            </button>
+            <button
+              className="split-button-cancel"
+              onClick={toggleSplitButtons}>
+              Cancel
+            </button>
+            <button className="split-button" onClick={handleLoadWorkout}>
+              From library
+            </button>
+          </div>
+        )}
         {user.accountType === "admin" && (
           <button className="toggle-admin-button" onClick={toggleAdmin}>
             Toggle Admin Options
