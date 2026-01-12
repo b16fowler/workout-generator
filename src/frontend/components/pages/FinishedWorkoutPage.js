3d1154048f1e86d2c0240fce91be04c4aae4813b
Brett Fowler <b16fowler@gmail.com>
2026-01-12 10:55:12 -0500

    Created new state variable to reload completed workouts (not finished, still need to reload images from workout)

--- src/frontend/components/pages/FinishedWorkoutPage.js
+++ src/frontend/components/pages/FinishedWorkoutPage.js
@@ -8,18 +8,29 @@
 
 import Header from "../other/Header";
 import Footer from "../other/Footer";
+import { useState } from "react";
 import ReturnHomeButton from "../buttons/ReturnHomeButton";
 import SaveWorkoutButton from "../buttons/SaveWorkoutButton";
 import ReloadWorkoutButton from "../buttons/ReloadWorkoutButton";
 import LoadNewWorkoutButton from "../buttons/LoadNewWorkoutButton";
+import Workout from "../other/Workout";
 
 export default function FinishedWorkoutPage({ workout }) {
+  const [reload, setReload] = useState(false);
+
   return (
     <>
       <Header heading="Workout completed!" />
-      <SaveWorkoutButton workout={workout} />
-      <LoadNewWorkoutButton />
-      <ReloadWorkoutButton />
+      {!reload && <SaveWorkoutButton workout={workout} />}
+      {!reload && <LoadNewWorkoutButton />}
+      {!reload && (
+        <ReloadWorkoutButton
+          workout={workout}
+          reload={reload}
+          setReload={setReload}
+        />
+      )}
+      {reload && <Workout workout={workout} />}
       <ReturnHomeButton />
       <Footer />
     </>
