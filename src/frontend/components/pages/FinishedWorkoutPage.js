f4788d96aa446a367436bfa9c8398cb576b3c12d
Brett Fowler <b16fowler@gmail.com>
2026-01-14 09:59:58 -0500

    Changing logic to have WorkoutPage rendered instead of being built into page

--- src/frontend/components/pages/FinishedWorkoutPage.js
+++ src/frontend/components/pages/FinishedWorkoutPage.js
@@ -8,29 +8,18 @@
 
 import Header from "../other/Header";
 import Footer from "../other/Footer";
-import { useState } from "react";
 import ReturnHomeButton from "../buttons/ReturnHomeButton";
 import SaveWorkoutButton from "../buttons/SaveWorkoutButton";
 import ReloadWorkoutButton from "../buttons/ReloadWorkoutButton";
 import LoadNewWorkoutButton from "../buttons/LoadNewWorkoutButton";
-import Workout from "../other/WorkoutPage";
 
 export default function FinishedWorkoutPage({ workout }) {
-  const [reload, setReload] = useState(false);
-
   return (
     <>
       <Header heading="Workout completed!" />
-      {!reload && <SaveWorkoutButton workout={workout} />}
-      {!reload && <LoadNewWorkoutButton />}
-      {!reload && (
-        <ReloadWorkoutButton
-          workout={workout}
-          reload={reload}
-          setReload={setReload}
-        />
-      )}
-      {reload && <Workout workout={workout} />}
+      <SaveWorkoutButton workout={workout} />
+      <LoadNewWorkoutButton />
+      <ReloadWorkoutButton workout={workout} />
       <ReturnHomeButton />
       <Footer />
     </>
